63c57de1cb08ada00ad9156910c4088ee669b2ed
takumi0826 <takumi.s8.26@gmail.com>
2022-01-02 23:58:19 +0900

    編集画面

--- src/organisms/tasks/EditTask.tsx
+++ src/organisms/tasks/EditTask.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Params, useParams } from 'react-router';
+import { TaskInfo } from 'types/TaskType';
+
+const url = `http://localhost:8080/api`;
 
 const EditTask = () => {
-  return (
-    <div>
-      <p>edit</p>
-    </div>
-  );
+  const [todo, setTodo] = useState<TaskInfo | null>(null);
+  useEffect(() => {
+    void getTask();
+  }, []);
+  const getTask = async () => {
+    const id = useParams<{ id: string }>();
+    console.log(id);
+
+    const taskUrl = `${url}/one-task?id=${id as string}`;
+    await axios
+      .get(taskUrl)
+      .then((res: AxiosResponse<TaskInfo | null>) => {
+        console.log(res.data);
+        setTodo(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return <div>{todo && <p>{todo.title}</p>}</div>;
 };
 
 export default EditTask;
