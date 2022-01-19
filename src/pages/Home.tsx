f6fb136e109487b74e18c3bd75c959a7b32309d4
takumi0826 <takumi.s8.26@gmail.com>
2022-01-19 23:50:50 +0900

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import React from 'react';
 
 const Home = () => {
+  const send = async (str: string) => {
+    const url = `http://localhost:8080/api/mail`;
+    await axios
+      .post(url, str)
+      .then((res) => {
+        alert(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="">
       <h1>home</h1>
+      <button onClick={() => send("hello")}>mailtest</button>
     </div>
   );
 };
